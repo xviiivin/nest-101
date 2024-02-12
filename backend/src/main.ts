e941bec096c7b6f11808e34c1641ffc2c456bbae
xviiivin <wiwatwin1801@gmail.com>
2024-02-13 00:26:44 +0700

    swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const apiPath = 'api';
+
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Nest-js Swagger Example API')
+    .setDescription('Swagger Example API API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  // Swagger path: http://localhost:3200/api/docs
+  SwaggerModule.setup(`${apiPath}/docs`, app, document);
+
   await app.listen(3000);
 }
 bootstrap();
